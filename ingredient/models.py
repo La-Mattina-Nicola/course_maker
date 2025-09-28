d8b2c0d9c16ecf781bcef559f2590446803a3770
La-Mattina-Nicola <nicola.lamattina@outlook.com>
2025-09-28 22:53:38 +0200

    Add max_length to CharField in Ingredient and TypeIngredient models

--- ingredient/models.py
+++ ingredient/models.py
@@ -3,19 +3,19 @@ from django.db import models
 # Create your models here.
 
 class TypeIngredient(models.Model):
-    name = models.CharField()
+    name = models.CharField(max_length=50)
     
     def __str__(self):
         return self.name
     
 class Ingredient(models.Model):
-    name = models.CharField()
+    name = models.CharField(max_length=50)
     type = models.ForeignKey(TypeIngredient, related_name="ingredient", on_delete=models.SET_NULL, null=True)
     def __str__(self):
         return self.name
     
 class Recette(models.Model):
-    name = models.CharField()
+    name = models.CharField(max_length=300)
     ingredients = models.ManyToManyField(Ingredient, related_name="recettes")
     def __str__(self):
         return self.name
