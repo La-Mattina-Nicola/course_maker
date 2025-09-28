d34c579310833a3ff74ff2d219d80bdddbca5ef0
La-Mattina-Nicola <nicola.lamattina@outlook.com>
2025-09-29 00:45:51 +0200

    create recette wip

--- ingredient/views.py
+++ ingredient/views.py
@@ -4,7 +4,8 @@ from rest_framework.viewsets import ModelViewSet
 from .models import TypeIngredient, Ingredient, Recette
 from .serializers import TypeIngredientSerializer, IngredientSerializer, RecetteSerializer
 from rest_framework.permissions import IsAuthenticated
-
+from rest_framework.decorators import action
+from rest_framework.response import Response
 # Create your views here.
 
 # return all
@@ -23,3 +24,26 @@ class RecetteViewSet(ModelViewSet):
     permission_classes = [IsAuthenticated]
     queryset = Recette.objects.all()
     serializer_class = RecetteSerializer
+    
+    @action(
+    detail=False,
+    methods=["post"],
+    permission_classes=[IsAuthenticated],
+    )
+    def createRecette(self, request, pk=None):
+        try:
+            name = request.data.get("name")
+            idIngredient = request.data.get("ingredients")
+
+            if not name or not idIngredient:
+                return Response({"message": "donnée manquante"}, status=400)
+
+            # idIngredient doit être une liste d'IDs
+            ingredients = Ingredient.objects.filter(id__in=idIngredient)
+            recette = Recette.objects.create(nom=name)
+            recette.ingredients.set(ingredients)  # ✅ Utilisation correcte
+            recette.save()
+
+            return Response({"message": "Recette créée avec succès"}, status=201)
+        except Exception as e:
+            return Response({"error": str(e)}, status=500)
