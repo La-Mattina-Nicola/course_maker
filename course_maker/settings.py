f5d6f9f2c63c8d774e1c799be3060124b06879f8
La-Mattina-Nicola <nicola.lamattina@outlook.com>
2025-09-28 23:04:43 +0200

    Refactor database settings to use dj_database_url for production configuration

--- course_maker/settings.py
+++ course_maker/settings.py
@@ -85,15 +85,17 @@ WSGI_APPLICATION = 'course_maker.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = ({
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    } if(DEBUG)
-    else {
-         "default": dj_database_url.config(
+DATABASES = (
+    {
+        "default": dj_database_url.config(
             default=env("DATABASE_URL"), conn_max_age=600, conn_health_checks=True
-        )
+        ),
+    }
+    if not DEBUG
+    else {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
         }
     }
 )
