e336003df8db64ed1d83e0883fb22f20f060222b
Nicola La Mattina <nicola.lamattina@outlook.com>
2025-10-01 13:52:33 +0200

    update model : family typerecette listecourse

--- ingredient/admin.py
+++ ingredient/admin.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
-from .models import TypeIngredient, Ingredient, Recette
+from .models import Famille, Ingredient, ItemListeCourse, ListeCourse, RecetteIngredient, Recette, TypeIngredient, TypeRecette, Unite
 # Register your models here.
 
 
-admin.site.register(TypeIngredient)    
-    
+admin.site.register(Famille)
 admin.site.register(Ingredient)
-
+admin.site.register(ItemListeCourse)
+admin.site.register(ListeCourse)
+admin.site.register(RecetteIngredient)
 admin.site.register(Recette)
+admin.site.register(TypeIngredient)
+admin.site.register(TypeRecette)
+admin.site.register(Unite)
+
--- ingredient/models.py
+++ ingredient/models.py
@@ -1,22 +1,68 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Famille(models.Model):
+    nom = models.CharField(max_length=100)
+    membres = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='familles')
+
+    def __str__(self):
+        return self.nom
 
 class TypeIngredient(models.Model):
     name = models.CharField(max_length=50)
-    
+
     def __str__(self):
         return self.name
-    
+
 class Ingredient(models.Model):
     name = models.CharField(max_length=50)
-    type = models.ForeignKey(TypeIngredient, related_name="ingredient", on_delete=models.SET_NULL, null=True)
+    type = models.ForeignKey(TypeIngredient, related_name="ingredients", on_delete=models.SET_NULL, null=True)
+
     def __str__(self):
         return self.name
-    
+
+class Unite(models.Model):
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+class TypeRecette(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
 class Recette(models.Model):
     name = models.CharField(max_length=300)
-    ingredients = models.ManyToManyField(Ingredient, related_name="recettes")
+    type = models.ForeignKey(TypeRecette, related_name="recettes", on_delete=models.SET_NULL, null=True)
+    ingredients = models.ManyToManyField(Ingredient, through='RecetteIngredient', related_name="recettes")
+
     def __str__(self):
         return self.name
-    
+
+class RecetteIngredient(models.Model):
+    recette = models.ForeignKey(Recette, on_delete=models.CASCADE)
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    quantity = models.FloatField()
+    unite = models.ForeignKey(Unite, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return f"{self.quantity} {self.unite.name if self.unite else ''} de {self.ingredient.name} pour {self.recette.name}"
+
+class ListeCourse(models.Model):
+    famille = models.ForeignKey(Famille, related_name='listes_courses', on_delete=models.CASCADE)
+    nom = models.CharField(max_length=100)
+    date_creation = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.nom} ({self.famille.nom})"
+
+class ItemListeCourse(models.Model):
+    liste = models.ForeignKey(ListeCourse, related_name='items', on_delete=models.CASCADE)
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    quantite = models.FloatField()
+    unite = models.ForeignKey(Unite, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return f"{self.quantite} {self.unite.name if self.unite else ''} de {self.ingredient.name}"
