35f390a59dd07d5ecaf3e92b05e5b53f4ff55581
Nicola La Mattina <nicola.lamattina@outlook.com>
2025-10-08 14:53:37 +0200

    Update default pagination page size to 50 in REST_FRAMEWORK settings

--- course_maker/settings.py
+++ course_maker/settings.py
@@ -145,7 +145,7 @@ REST_FRAMEWORK = {
         "rest_framework.permissions.IsAuthenticated",
     ],
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
-    "PAGE_SIZE": 10,
+    "PAGE_SIZE": 50,
     "PAGE_SIZE_QUERY_PARAM": "page_size",
 }
 
